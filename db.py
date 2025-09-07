7246b409b579bdf5747913c08e268a5e0176eb2f
Vaishnavi Verma <129358319+Vaishnavi-Verma-21@users.noreply.github.com>
2025-09-07 21:40:34 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,31 +1,28 @@
-import streamlit as st
 from astrapy import DataAPIClient
+from dotenv import load_dotenv
+import streamlit as st
+import os
 
-# Fetch secrets
-ASTRA_DB_ID = st.secrets["ASTRA_DB_ID"]
-ASTRA_DB_TOKEN = st.secrets["ASTRA_DB_APPLICATION_TOKEN"]
-KEYSPACE_NAME = st.secrets["ASTRA_KEYSPACE"]
+load_dotenv()
+
+ENDPOINT = os.getenv("ASTRA_ENDPOINT")
+TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+
 
 @st.cache_resource
 def get_db():
-    # Initialize Astra DB client
-    client = DataAPIClient(
-        astra_database_id=ASTRA_DB_ID,
-        astra_application_token=ASTRA_DB_TOKEN
-    )
-    # Connect to keyspace
-    db = client.get_keyspace(KEYSPACE_NAME)
+    client = DataAPIClient(TOKEN)
+    db = client.get_database_by_api_endpoint(ENDPOINT)
     return db
 
 db = get_db()
-
-# Collections
 collection_names = ["personal_data", "notes"]
+
 for collection in collection_names:
     try:
         db.create_collection(collection)
     except:
         pass
-
+    
 personal_data_collection = db.get_collection("personal_data")
 notes_collection = db.get_collection("notes")
